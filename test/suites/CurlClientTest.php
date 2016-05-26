fe7515ffb266cff0326ab5d3729848be2c77b98e
Soufiane Ghzal <sghzal@gmail.com>
2016-05-26 09:23:23 +0200

    add tests for curl exception

--- test/suites/CurlClientTest.php
+++ test/suites/CurlClientTest.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ResponseInterface;
 use Serps\Core\Cookie\ArrayCookieJar;
 use Serps\Core\Cookie\Cookie;
 use Serps\Core\Http\HttpClientInterface;
+use Serps\Core\Http\Proxy;
 use Serps\Core\Http\SearchEngineResponse;
 use Serps\HttpClient\CurlClient\CookieFile;
 use Serps\HttpClient\CurlClient;
@@ -20,11 +21,28 @@ use Zend\Diactoros\Response;
  * @covers Serps\HttpClient\CurlClient\Curl
  * @covers Serps\HttpClient\CurlClient\ResponseBuilder
  * @covers Serps\HttpClient\CurlClient\CookieFile
+ * @covers Serps\HttpClient\CurlClient\CurlException
  */
 class CurlClientTest extends HttpClientTestsCase
 {
     public function getHttpClient()
     {
         return new CurlClient();
+    }
+
+    public function testCurlException()
+    {
+
+        $client = $this->getHttpClient();
+        $request = new Request('http://httpbin.org/get', 'GET');
+
+        try {
+            $client->sendRequest($request, Proxy::createFromString('1111.0.0.0:50'));
+            $this->fail('no exception thrown');
+        } catch (CurlClient\CurlException $e) {
+            $this->assertEquals(5, $e->getCurlErrCode());
+            $this->assertNotEmpty($e->getCurlErrMessage());
+        }
+
     }
 }
